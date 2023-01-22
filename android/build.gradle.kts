7ca0b950107c6302343f1fedfe4d523e872f366c
David Laurell <david.laurell@gmail.com>
2023-01-22 15:04:02 +0100

    Fix application id

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     compileSdk = 33
     defaultConfig {
-        applicationId = "com.example.android"
+        applicationId = "net.daverix.py2kted"
         minSdk = 24
         targetSdk = 33
         versionCode = 1
